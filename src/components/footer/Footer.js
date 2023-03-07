8c44a5f50d354803ddb047ef1d388e5041f84317
vimal_ROG <vimal@cdp360.com>
2023-03-07 19:36:30 +0530

    logo change for cdp360 in

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,15 +4,16 @@ function Footer() {
   return (
     <div
       style={{
+        backgroundColor: "#00000000",
         textAlign: "end",
         padding: "1%",
-        paddingBottom: "0%",
+        paddingBottom: "1%",
         fontSize: "13px",
         fontWeight: "600",
         color: "gray",
       }}
     >
-      Powered by GoAdem
+      @Copyright CDP360 2023 -- All Rights Reserved
     </div>
   );
 }
